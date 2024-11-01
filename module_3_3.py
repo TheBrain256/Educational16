7fdf344bb08062bfa935a4b9ee27bd4825008cd7
TheBrain256 <Shimoro2576@gmail.com>
2024-11-01 13:19:17 +0300

    Add files via upload

---
+++ module_3_3.py
@@ -0,0 +1,15 @@
+def print_params(a=1, b='string', c=True):
+    print()
+    print(a)
+    print(c)
+    print(a,b)
+    print(a,b,c)
+print_params()
+print_params(b = 25)
+print_params(c = [1,2,3])
+values_list = (25, False, 'Книга')
+values_dict = {'a' : 3, 'b' : True, 'c' : 'Альбом' }
+print_params(*values_list)
+print_params(**values_dict)
+values_list_2 = (15, 'Слово')
+print_params(*values_list_2, 42)
